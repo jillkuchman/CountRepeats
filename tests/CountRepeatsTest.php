17470a9619cc5ddae415e3149adbef42f7332589
Jill Kuchman <jillkuchman@gmail.com>
2015-03-13 10:07:23 -0700

    Add test: exact match, one letter

--- tests/CountRepeatsTest.php
+++ tests/CountRepeatsTest.php
@@ -4,7 +4,22 @@
 
 	Class CountRepeatsTest extends PHPUnit_Framework_TestCase
 	{
-		
+
+		function test_ExactMatch_OneLetter
+		{
+			//Arrange
+			$exact_match_one_letter = new CountRepeats;
+			$string_to_check = 'a';
+			$word_to_count = 'a';
+
+			//Act
+			$result = $exact_match_one_letter->countRepeatsCounter($string_to_check, $word_to_count);
+
+			//Assert
+			$this->assertEquals(1, $result);
+
+		}
+
 	}
 
 ?>
