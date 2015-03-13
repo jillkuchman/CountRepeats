98835188a1ff1cd41de36a770473d1dd9e2dc312
Jill Kuchman <jillkuchman@gmail.com>
2015-03-13 11:35:46 -0700

    Add twig, silex dependencies to app.php

--- app/app.php
+++ app/app.php
@@ -3,4 +3,9 @@
 	require_once __DIR__."/../vendor/autoload.php";
 	require_once __DIR__."/../src/CountRepeats.php";
 
+	$app = new Silex\Application();
+
+	$app->register(new Silex\Provider\TwigServiceProvider(), array(
+		'twig.path' => __DIR__."/../views"
+	));
 ?>
