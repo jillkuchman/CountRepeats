b634725ec4d1f5450bce9b395fa74e377d58c3e1
Jill Kuchman <jillkuchman@gmail.com>
2015-03-13 10:27:43 -0700

    Add method: exact match, one letter, case-insensitive

--- src/CountRepeats.php
+++ src/CountRepeats.php
@@ -5,7 +5,10 @@
 
 		function countRepeatsCounter($input_string, $input_word)
 		{
-			if ($input_string === $input_word){
+			$input_string_lc = strtolower($input_string);
+			$input_word_lc = strtolower($input_word);
+
+			if ($input_string_lc === $input_word_lc){
 				return 1;
 			}
 			
