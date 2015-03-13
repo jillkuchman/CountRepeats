85d8f0e079b377c15931a82610916e789dc12c78
Jill Kuchman <jillkuchman@gmail.com>
2015-03-13 10:53:04 -0700

    Add method: exact match, multiple instances of word in string

--- src/CountRepeats.php
+++ src/CountRepeats.php
@@ -5,12 +5,21 @@
 
 		function countRepeatsCounter($input_string, $input_word)
 		{
-			$input_string_lc = strtolower($input_string);
-			$input_word_lc = strtolower($input_word);
 
-			if ($input_string_lc === $input_word_lc){
-				return 1;
+			//this line turns the input string into an array of words
+			$input_array_of_words = explode(" ", $input_string);
+
+			//this line initalizes the word count at 0
+			$count = 0;
+
+			//this function will cycle through the array of the input string to see if it matches input word. If they match, it increases the count by 1.
+			foreach ($input_array_of_words as $word_to_check) {
+				if (strtolower($word_to_check) === strtolower($input_word)) {
+					$count ++;
+				}
 			}
+
+			return $count;
 			
 		}
 
