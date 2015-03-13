5cc71ac2832df71675eaea99dfc1eac6a40622f4
Jill Kuchman <jillkuchman@gmail.com>
2015-03-13 13:31:20 -0700

    Add input from form.twig to results.twig

--- app/app.php
+++ app/app.php
@@ -9,8 +9,14 @@
 		'twig.path' => __DIR__."/../views"
 	));
 
-	$app->get("/", function() use ($app) {
+	$app->get("/", function() use($app) {
 		return $app['twig']->render('form.twig');
+	});
+
+	$app->get("/wordcount", function() use($app) {
+		$count_repeats = new RepeatCounter;
+		$word_count = $count_repeats->countRepeats($_GET['user_sentence_input'], $_GET['user_word_input']);
+		return $app['twig']->render('results.twig', array('total_word_count' => $word_count, 'user_sentence' => $_GET['user_sentence_input'], 'user_word' => $_GET['user_word_input']));
 	});
 
 	return $app;
