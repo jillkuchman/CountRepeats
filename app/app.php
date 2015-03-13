94663a73e7c78434940a217eb970232a29aaa3e1
Jill Kuchman <jillkuchman@gmail.com>
2015-03-13 11:37:53 -0700

    Add root route to form.twig

--- app/app.php
+++ app/app.php
@@ -8,4 +8,10 @@
 	$app->register(new Silex\Provider\TwigServiceProvider(), array(
 		'twig.path' => __DIR__."/../views"
 	));
+
+	$app->get('/', function() use ($app) {
+		return $app['twig']->render(form.twig);
+	});
+
+	
 ?>
